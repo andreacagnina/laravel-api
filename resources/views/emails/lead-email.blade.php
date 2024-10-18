2e358c44bc3f909ab27e04d52fb188b020ce18a3
andreacagnina <amirkal@libero.it>
2024-10-18 11:24:10 +0200

    [feat] rem sect lead-email.blade.php

--- resources/views/emails/lead-email.blade.php
+++ resources/views/emails/lead-email.blade.php
@@ -1,20 +1,17 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container my-3">
-        <div class="row">
-            <div class="col-12">
-                <div class="content">
-                    <h1 class="text-center">Hai una richiesta da trattare!</h1>
-                </div>
+{{-- GLI STILI VANNO MESSI INLINE --}}
+<div class="container my-3">
+    <div class="row">
+        <div class="col-12">
+            <div class="content">
+                <h1 class="text-center">Hai una richiesta da trattare!</h1>
             </div>
-            <div class="col-12">
-                <div class="content">
-                    <p><strong>Nome Completo: </strong> {{ $lead->name }} {{ $lead->surname }}</p>
-                    <p> <strong>Email: </strong> {{ $lead->email }}</p>
-                    <p> <strong>Messaggio: </strong> {{ $lead->message }}</p>
-                </div>
+        </div>
+        <div class="col-12">
+            <div class="content">
+                <p><strong>Nome Completo: </strong> {{ $lead->name }} {{ $lead->surname }}</p>
+                <p> <strong>Email: </strong> {{ $lead->email }}</p>
+                <p> <strong>Messaggio: </strong> {{ $lead->message }}</p>
             </div>
         </div>
     </div>
-@endsection
+</div>
